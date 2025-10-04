294dc6d9b23b6b8905cf92bb5a8674ca1a3bef20
Simon Bumiller <simon@bumiller.me>
2025-10-05 01:28:40 +0200

    Explicitly declared spring context class in test

--- inter/src/test/java/com/rere/server/ReplicReadServerApplicationTests.java
+++ inter/src/test/java/com/rere/server/ReplicReadServerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.rere.server;
 
+import com.rere.server.inter.ReplicReadServerApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ReplicReadServerApplication.class)
 class ReplicReadServerApplicationTests {
 
     @Test
