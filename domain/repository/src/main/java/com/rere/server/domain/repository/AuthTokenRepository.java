c748b97274f893caa9177cb6fa516856d0ce1808
Simon Bumiller <simon@bumiller.me>
2025-10-05 23:15:21 +0200

    Added domain-repositories

---
+++ domain/repository/src/main/java/com/rere/server/domain/repository/AuthTokenRepository.java
@@ -0,0 +1,9 @@
+package com.rere.server.domain.repository;
+
+import com.rere.server.domain.model.account.AuthToken;
+
+/**
+ * Repository for {@link AuthToken} models.
+ */
+public interface AuthTokenRepository extends BaseRepository<AuthToken> {
+}
---
+++ domain/repository/src/main/java/com/rere/server/domain/repository/BaseRepository.java
@@ -0,0 +1,34 @@
+package com.rere.server.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Base repository that provides basic CRUD-methods for other repositories.
+ *
+ * @param <M> The model that this repository provides access to.
+ */
+public interface BaseRepository<M> {
+
+    /**
+     * Gets all saved models.
+     * @return The list of all models.
+     */
+    List<M> getAll();
+
+    /**
+     * Gets a model by its specific id.
+     * @param id The id of the model.
+     * @return The model, or an empty {@link Optional}.
+     */
+    Optional<M> getById(UUID id);
+
+    /**
+     * Saves a given model.
+     * @param m The model to save.
+     * @return The model, because it might have been changed in the process of saving.
+     */
+    M save(M m);
+
+}
