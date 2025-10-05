7ba0b181f4f55819ed5c8ac85eb142129332b8e4
Simon Bumiller <simon@bumiller.me>
2025-10-06 01:52:31 +0200

    Added period start field to ReplicLimitConfig

--- domain/model/src/main/java/com/rere/server/domain/model/config/ReplicLimitConfig.java
+++ domain/model/src/main/java/com/rere/server/domain/model/config/ReplicLimitConfig.java
@@ -3,6 +3,7 @@ package com.rere.server.domain.model.config;
 import lombok.Data;
 import lombok.NonNull;
 
+import java.time.Instant;
 import java.time.Period;
 
 /**
@@ -21,5 +22,11 @@ public class ReplicLimitConfig {
      * The count of replics that can be created
      */
     private int count;
+
+    /**
+     * The timestamp on which the current period settings were started.
+     */
+    @NonNull
+    private Instant periodStart;
 
 }
