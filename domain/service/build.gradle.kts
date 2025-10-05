4273a56a3400533327420bb5b3cf3f3e0e19b010
Simon Bumiller <simon@bumiller.me>
2025-10-05 23:25:17 +0200

    Added empty :domain:service directory

---
+++ domain/service/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.convention.java)
+    alias(libs.plugins.convention.test)
+    alias(libs.plugins.convention.spring)
+}
+
+dependencies {
+    implementation(project(":domain:model"))
+    implementation(project(":domain:repository"))
+}
