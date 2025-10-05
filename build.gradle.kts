56a57d6beb0766c9217e16e0ec8201ad7bc8c5f8
Simon Bumiller <simon@bumiller.me>
2025-10-05 02:15:43 +0200

    Fixed missing spring plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     java
     id("org.sonarqube") version "6.3.1.5724"
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.rere"
--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 repositories {
