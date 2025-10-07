4d6913c9a18d012edef5ae43805ba1e180175340
Simon Bumiller <simon@bumiller.me>
2025-10-07 16:10:41 +0200

    Fixed compile errors in other tests

--- domain/model/src/test/java/com/rere/server/domain/model/account/AuthTokenTest.java
+++ domain/model/src/test/java/com/rere/server/domain/model/account/AuthTokenTest.java
@@ -34,7 +34,7 @@ class AuthTokenTest {
         Account account = createAccount(UUID.randomUUID());
         AuthToken token = new AuthToken(UUID.randomUUID(), Instant.now(), UUID.randomUUID(), Instant.ofEpochSecond(instant), account, AuthTokenType.REFRESH_TOKEN, "", false);
 
-        Assertions.assertFalse(token.isValid(account, AuthTokenType.REFRESH_TOKEN, now));
+        Assertions.assertFalse(token.isValid(AuthTokenType.REFRESH_TOKEN, now));
     }
 
     @ParameterizedTest
@@ -46,10 +46,9 @@ class AuthTokenTest {
     void isValidWorksForNonExpired(long instant) {
         Instant now = Instant.ofEpochSecond(75847300);
 
-        Account account = createAccount(UUID.randomUUID());
-        AuthToken token = new AuthToken(UUID.randomUUID(), Instant.now(), UUID.randomUUID(), Instant.ofEpochSecond(instant), account, AuthTokenType.REFRESH_TOKEN, "", false);
+        AuthToken token = new AuthToken(UUID.randomUUID(), Instant.now(), UUID.randomUUID(), Instant.ofEpochSecond(instant), createAccount(UUID.randomUUID()), AuthTokenType.REFRESH_TOKEN, "", false);
 
-        Assertions.assertTrue(token.isValid(account, AuthTokenType.REFRESH_TOKEN, now));
+        Assertions.assertTrue(token.isValid(AuthTokenType.REFRESH_TOKEN, now));
     }
 
     @Test
@@ -61,26 +60,8 @@ class AuthTokenTest {
         AuthToken invalidatedToken = new AuthToken(UUID.randomUUID(), Instant.now(), UUID.randomUUID(), expiration, account, AuthTokenType.REFRESH_TOKEN, null, true);
         AuthToken notInvalidatedToken = new AuthToken(UUID.randomUUID(), Instant.now(), UUID.randomUUID(), expiration, account, AuthTokenType.REFRESH_TOKEN, null, false);
 
-        Assertions.assertFalse(invalidatedToken.isValid(account, AuthTokenType.REFRESH_TOKEN, now));
-        Assertions.assertTrue(notInvalidatedToken.isValid(account, AuthTokenType.REFRESH_TOKEN, now));
-    }
-
-    @Test
-    void isValidUsesAccountId() {
-        Instant now = Instant.ofEpochSecond(75847300);
-        Instant expiration = now.plusSeconds(1000);
-        UUID id1 = UUID.fromString("b6c241de-b456-4a74-87c8-f3021e4ec825");
-        UUID id2 = UUID.fromString("dfc8f407-224a-4ab5-b976-16fd11572863");
-        UUID id3 = UUID.fromString("7c9dfc5f-bd16-4dd3-8b0d-a9c01a2f453a");
-        Account acc1 = createAccount(id1);
-        Account acc2 = createAccount(id2);
-        Account acc3 = createAccount(id3);
-
-        AuthToken token1 = new AuthToken(UUID.randomUUID(), Instant.now(), UUID.randomUUID(), expiration, acc1, AuthTokenType.REFRESH_TOKEN, "", false);
-
-        Assertions.assertTrue(token1.isValid(acc1, AuthTokenType.REFRESH_TOKEN, now));
-        Assertions.assertFalse(token1.isValid(acc2, AuthTokenType.REFRESH_TOKEN, now));
-        Assertions.assertFalse(token1.isValid(acc3, AuthTokenType.REFRESH_TOKEN, now));
+        Assertions.assertFalse(invalidatedToken.isValid(AuthTokenType.REFRESH_TOKEN, now));
+        Assertions.assertTrue(notInvalidatedToken.isValid(AuthTokenType.REFRESH_TOKEN, now));
     }
 
     @Test
@@ -91,8 +72,8 @@ class AuthTokenTest {
 
         AuthToken tokenForRefresh = new AuthToken(UUID.randomUUID(), Instant.now(), UUID.randomUUID(), expiration, account, AuthTokenType.REFRESH_TOKEN, "", false);
 
-        Assertions.assertTrue(tokenForRefresh.isValid(account, AuthTokenType.REFRESH_TOKEN, now));
-        Assertions.assertFalse(tokenForRefresh.isValid(account, AuthTokenType.EMAIL_VERIFICATION, now));
+        Assertions.assertTrue(tokenForRefresh.isValid(AuthTokenType.REFRESH_TOKEN, now));
+        Assertions.assertFalse(tokenForRefresh.isValid(AuthTokenType.EMAIL_VERIFICATION, now));
     }
 
 }
