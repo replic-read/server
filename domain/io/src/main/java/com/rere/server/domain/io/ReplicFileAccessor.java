398282726c7b0930bd15f25485fbb809267f7416
Simon Bumiller <simon@bumiller.me>
2025-10-08 18:04:57 +0200

    Update file accessor interface

--- domain/io/src/main/java/com/rere/server/domain/io/ReplicFileAccessor.java
+++ domain/io/src/main/java/com/rere/server/domain/io/ReplicFileAccessor.java
@@ -1,9 +1,9 @@
 package com.rere.server.domain.io;
 
 import com.rere.server.domain.model.exception.NotFoundException;
+import com.rere.server.domain.model.replic.ReplicFileData;
 
 import java.io.File;
-import java.io.InputStream;
 import java.util.UUID;
 
 /**
@@ -19,21 +19,12 @@ public interface ReplicFileAccessor {
     File createForReplic(UUID replicId);
 
     /**
-     * Gets the size of the replic content-file in bytes.
+     * Gets the file data for the replic.
      *
      * @param replicId The id of the replic.
-     * @return The data size in bytes.
+     * @return The file data.
      * @throws NotFoundException If the file could not be found.
      */
-    long getDataSize(UUID replicId) throws NotFoundException;
-
-    /**
-     * Gets the file stream for a specific replic id.
-     *
-     * @param replicId The id of the replic tog et the file stream.
-     * @return The stream containing the file data.
-     * @throws NotFoundException If the file could not be found.
-     */
-    InputStream getDataStream(UUID replicId) throws NotFoundException;
+    ReplicFileData getFileData(UUID replicId) throws NotFoundException;
 
 }
