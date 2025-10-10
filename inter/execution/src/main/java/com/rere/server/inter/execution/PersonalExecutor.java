81687763c97252bc666fba51eb89df328b1c462e
Simon Bumiller <simon@bumiller.me>
2025-10-11 01:10:18 +0200

    Added executor for account info

---
+++ inter/execution/src/main/java/com/rere/server/inter/execution/PersonalExecutor.java
@@ -0,0 +1,31 @@
+package com.rere.server.inter.execution;
+
+import com.rere.server.inter.execution.dto.request.UpdateAccountRequest;
+import com.rere.server.inter.execution.dto.response.AccountResponse;
+import com.rere.server.inter.execution.dto.response.QuotaProgressResponse;
+
+/**
+ * Request executor for requests for the account of the client.
+ */
+public interface PersonalExecutor {
+
+    /**
+     * Executor for GET /me/.
+     * @return The response body.
+     */
+    AccountResponse getMe();
+
+    /**
+     * Executor for POST /me/.
+     * @param request The request body.
+     * @return The response body.
+     */
+    AccountResponse updateMe(UpdateAccountRequest request);
+
+    /**
+     * Executor for GET /me/quota/.
+     * @return The response body.
+     */
+    QuotaProgressResponse getQuotaProgress();
+
+}
