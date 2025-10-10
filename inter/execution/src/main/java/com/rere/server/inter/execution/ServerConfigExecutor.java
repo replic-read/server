ef8ec251dd8e416fa23a85bf8d6419f60a86faed
Simon Bumiller <simon@bumiller.me>
2025-10-11 01:10:57 +0200

    Added executor for server-config

---
+++ inter/execution/src/main/java/com/rere/server/inter/execution/ServerConfigExecutor.java
@@ -0,0 +1,24 @@
+package com.rere.server.inter.execution;
+
+import com.rere.server.inter.execution.dto.request.ServerConfigRequest;
+import com.rere.server.inter.execution.dto.response.ServerConfigResponse;
+
+/**
+ * Executor for requests with the server config.
+ */
+public interface ServerConfigExecutor {
+
+    /**
+     * Executor for GET /server-config/.
+     * @return The response body.
+     */
+    ServerConfigResponse getServerConfig();
+
+    /**
+     * Endpoint for POST /server-config/.
+     * @param request The request body.
+     * @return The response body.
+     */
+    ServerConfigResponse setServerConfig(ServerConfigRequest request);
+
+}
---
+++ inter/execution/src/main/java/com/rere/server/inter/execution/impl/ServerConfigExecutorImpl.java
@@ -0,0 +1,69 @@
+package com.rere.server.inter.execution.impl;
+
+import com.rere.server.domain.model.config.AuthUserGroup;
+import com.rere.server.domain.model.config.ReplicLimitConfig;
+import com.rere.server.domain.model.config.ServerConfig;
+import com.rere.server.domain.model.impl.ReplicLimitConfigImpl;
+import com.rere.server.domain.model.impl.ServerConfigImpl;
+import com.rere.server.domain.service.AccountService;
+import com.rere.server.domain.service.AuthenticationService;
+import com.rere.server.domain.service.QuotaService;
+import com.rere.server.domain.service.ReplicService;
+import com.rere.server.domain.service.ReportService;
+import com.rere.server.domain.service.ServerConfigService;
+import com.rere.server.inter.authorization.Authorizer;
+import com.rere.server.inter.execution.AbstractExecutor;
+import com.rere.server.inter.execution.ServerConfigExecutor;
+import com.rere.server.inter.execution.dto.request.ServerConfigRequest;
+import com.rere.server.inter.execution.dto.response.ServerConfigResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Period;
+
+import static com.rere.server.inter.execution.mapper.EnumMapper.mapToEnum;
+import static com.rere.server.inter.execution.mapper.EnumMapper.mapToString;
+
+@Component
+public class ServerConfigExecutorImpl extends AbstractExecutor implements ServerConfigExecutor {
+    @Autowired
+    protected ServerConfigExecutorImpl(AccountService accountService, AuthenticationService authService, ReplicService replicService, ReportService reportService, ServerConfigService configService, QuotaService quotaService, Authorizer authorizer, String baseUrl) {
+        super(accountService, authService, replicService, reportService, configService, quotaService, authorizer);
+    }
+
+    private static ServerConfigResponse createServerConfigResponse(ServerConfig config) {
+        return new ServerConfigResponse(mapToString(config.getCreateReplicsGroup()), mapToString(config.getAccessReplicsGroup()),
+                mapToString(config.getCreateReportsGroup()),
+                config.getMaximumActivePeriod() != null ? config.getMaximumActivePeriod().toString() : null,
+                config.getLimit() != null ? config.getLimit().getPeriod().toString() : null,
+                config.getLimit() != null ? config.getLimit().getCount() : null,
+                config.getLimit() != null ? config.getLimit().getPeriodStart().toString() : null,
+                config.isAllowAccountCreation());
+    }
+
+    @Override
+    public ServerConfigResponse getServerConfig() {
+        return createServerConfigResponse(configService.get());
+    }
+
+    @Override
+    public ServerConfigResponse setServerConfig(ServerConfigRequest request) {
+        authorizer.requireChangeServerConfig(getAuth());
+
+        ReplicLimitConfig newLimit = (request.replicLimitCount() != null &&
+                                      request.replicLimitPeriod() != null)
+                ? new ReplicLimitConfigImpl(Period.parse(request.replicLimitPeriod()),
+                null, request.replicLimitCount()) : null;
+        ServerConfig newConfig = new ServerConfigImpl(
+                mapToEnum(request.createReplicGroup(), AuthUserGroup.class),
+                mapToEnum(request.accessReplicGroup(), AuthUserGroup.class),
+                mapToEnum(request.createReportGroup(), AuthUserGroup.class),
+                request.allowSignup(),
+                newLimit,
+                request.maximumExpirationPeriod() != null ? Period.parse(request.maximumExpirationPeriod()) : null
+        );
+        configService.save(newConfig);
+
+        return createServerConfigResponse(configService.get());
+    }
+}
