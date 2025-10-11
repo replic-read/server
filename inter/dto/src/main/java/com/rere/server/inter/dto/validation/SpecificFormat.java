45d1dd6412c128dc6cfa3a368a960782200e78fb
Simon Bumiller <simon@bumiller.me>
2025-10-11 20:03:19 +0200

    Removed validation methods from SpecificFormat

--- inter/dto/src/main/java/com/rere/server/inter/dto/validation/SpecificFormat.java
+++ inter/dto/src/main/java/com/rere/server/inter/dto/validation/SpecificFormat.java
@@ -1,9 +1,6 @@
 package com.rere.server.inter.dto.validation;
 
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.time.Period;
-import java.time.format.DateTimeParseException;
 
 /**
  * Specific formats that can't be validated with a regex.
@@ -23,47 +20,6 @@ public enum SpecificFormat {
     /**
      * A positive integer.
      */
-    POSITIVE_INTEGER;
-
-    private static boolean validateJavaPeriod(Object value) {
-        try {
-            Period.parse(value.toString());
-            return true;
-        } catch (DateTimeParseException e) {
-            return false;
-        }
-    }
-
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private static boolean validateUrl(Object value) {
-        try {
-            URI.create(value.toString()).toURL();
-            return true;
-        } catch (MalformedURLException e) {
-            return false;
-        }
-    }
-
-    /**
-     * ValidChecks whether the value is valid according to the validation type.
-     * @param value The value.
-     * @return Whether the value is valid.
-     */
-    public boolean isValid(Object value) {
-        return switch (this) {
-            case URL -> validateUrl(value);
-            case JAVA_PERIOD -> validateJavaPeriod(value);
-            case POSITIVE_INTEGER -> validatePositiveInteger(value);
-        };
-    }
-
-    private boolean validatePositiveInteger(Object value) {
-        try {
-            Integer.valueOf(value.toString());
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
+    POSITIVE_INTEGER
 
 }
