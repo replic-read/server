4799cf1e492cdc6b7a3dce49e4ebf91460af0af7
Simon Bumiller <simon@bumiller.me>
2025-10-12 19:06:47 +0200

    Added missing @Value

--- inter/execution/src/main/java/com/rere/server/inter/execution/impl/ServerConfigExecutorImpl.java
+++ inter/execution/src/main/java/com/rere/server/inter/execution/impl/ServerConfigExecutorImpl.java
@@ -17,6 +17,7 @@ import com.rere.server.inter.dto.response.ServerConfigResponse;
 import com.rere.server.inter.execution.AbstractExecutor;
 import com.rere.server.inter.execution.ServerConfigExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -29,7 +30,7 @@ import static com.rere.server.inter.dto.mapper.EnumMapper.mapToString;
 @Component
 public class ServerConfigExecutorImpl extends AbstractExecutor implements ServerConfigExecutor {
     @Autowired
-    protected ServerConfigExecutorImpl(AccountService accountService, AuthenticationService authService, ReplicService replicService, ReportService reportService, ServerConfigService configService, QuotaService quotaService, Authorizer authorizer, String baseUrl) {
+    protected ServerConfigExecutorImpl(AccountService accountService, AuthenticationService authService, ReplicService replicService, ReportService reportService, ServerConfigService configService, QuotaService quotaService, Authorizer authorizer, @Value("${rere.baseurl") String baseUrl) {
         super(accountService, authService, replicService, reportService, configService, quotaService, authorizer);
     }
 
