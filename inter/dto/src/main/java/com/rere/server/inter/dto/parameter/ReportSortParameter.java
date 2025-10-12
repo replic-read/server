ca69bb6ae3b1012ba3ef864441874e0381b9163d
Simon Bumiller <simon@bumiller.me>
2025-10-12 16:37:38 +0200

    Moved Pair outside Fixed report-user comparator

--- inter/dto/src/main/java/com/rere/server/inter/dto/parameter/ReportSortParameter.java
+++ inter/dto/src/main/java/com/rere/server/inter/dto/parameter/ReportSortParameter.java
@@ -14,9 +14,24 @@ public enum ReportSortParameter implements SortParameter<Report> {
     public Comparator<Report> getComparator(SortDirectionParameter direction) {
         Comparator<Report> comparator = switch (this) {
             case DATE -> Comparator.comparing(Report::getCreationTimestamp);
-            case USER -> Comparator.comparing(Report::getAuthorId);
+            case USER -> new ReportUserComparator();
         };
 
         return SortDirectionParameter.ASCENDING.equals(direction) ? comparator : comparator.reversed();
+    }
+
+    // Compares based on the user id.
+    // 'null' is treated as low.
+    private static class ReportUserComparator implements Comparator<Report> {
+        @Override
+        public int compare(Report r1, Report r2) {
+            if (r2.getAuthorId() == null && r1.getAuthorId() == null) {
+                return 0;
+            } else if (r2.getAuthorId() == null) {
+                return 1;
+            } else {
+                return r1.getAuthorId().compareTo(r2.getAuthorId());
+            }
+        }
     }
 }
