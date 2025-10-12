6d502559a83f42cb379f18a8eb73091153a422b4
Simon Bumiller <simon@bumiller.me>
2025-10-12 15:09:09 +0200

    Added tests for CredentialsRequest validation

--- inter/dto/src/main/java/com/rere/server/inter/dto/request/CredentialsRequest.java
+++ inter/dto/src/main/java/com/rere/server/inter/dto/request/CredentialsRequest.java
@@ -5,10 +5,14 @@ import com.rere.server.inter.dto.validation.ValidationMetadata;
 
 /**
  * Request body with credentials.
+ * <br><br>
+ * Note: Although these are fixed formats, we do not perform any validation.
+ *      If in the future the formats for email/username/password get stricter,
+ *      users who followed the old strictness would not be able to log in anymore.
  */
 public record CredentialsRequest(
-        @ValidationMetadata(value = FieldType.ACCOUNT_EMAIL, required = false) String email,
-        @ValidationMetadata(value = FieldType.ACCOUNT_USERNAME, required = false) String username,
-        @ValidationMetadata(FieldType.ACCOUNT_PASSWORD) String password
+        @ValidationMetadata(value = FieldType.ACCOUNT_EMAIL, required = false, doValidate = false) String email,
+        @ValidationMetadata(value = FieldType.ACCOUNT_USERNAME, required = false, doValidate = false) String username,
+        @ValidationMetadata(value = FieldType.ACCOUNT_PASSWORD, doValidate = false) String password
 ) {
 }
