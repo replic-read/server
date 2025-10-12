ccea508afbea192fa3770521cb212fdb30de6416
Simon Bumiller <simon@bumiller.me>
2025-10-12 15:03:42 +0200

    Added test for CreateReportRequest

---
+++ inter/dto/src/test/java/com/rere/server/inter/dto/request/CreateReportRequestTest.java
@@ -0,0 +1,21 @@
+package com.rere.server.inter.dto.request;
+
+import com.rere.server.inter.dto.error.ErrorResponseInfo;
+import com.rere.server.inter.dto.validation.AbstractValidationTest;
+
+class CreateReportRequestTest extends AbstractValidationTest<CreateReportRequest> {
+
+    @Override
+    protected CreateReportRequest[] getValidDtos() {
+        return new CreateReportRequest[]{
+                new CreateReportRequest("desc"),
+                new CreateReportRequest(""),
+                new CreateReportRequest(null),
+        };
+    }
+
+    @Override
+    protected Pair<CreateReportRequest, ? extends ErrorResponseInfo>[] getInvalidDtos() {
+        return new Pair[0];
+    }
+}
