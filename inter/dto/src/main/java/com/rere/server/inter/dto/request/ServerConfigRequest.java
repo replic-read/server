58910056b6df03a79cf11e460f72b153c60b86ec
Simon Bumiller <simon@bumiller.me>
2025-10-13 03:39:26 +0200

    Made some parameters required in ServerConfigRequest

--- inter/dto/src/main/java/com/rere/server/inter/dto/request/ServerConfigRequest.java
+++ inter/dto/src/main/java/com/rere/server/inter/dto/request/ServerConfigRequest.java
@@ -11,9 +11,9 @@ public record ServerConfigRequest(
         @ValidationMetadata(FieldType.CONFIG_CREATE_REPLIC_GROUP) @JsonProperty("create_replic_group") String createReplicGroup,
         @ValidationMetadata(FieldType.CONFIG_ACCESS_REPLIC_GROUP) @JsonProperty("access_replic_group") String accessReplicGroup,
         @ValidationMetadata(FieldType.CONFIG_CREATE_REPORT_GROUP) @JsonProperty("create_report_group") String createReportGroup,
-        @ValidationMetadata(FieldType.CONFIG_MAX_EXP) @JsonProperty("maximum_expiration_period") String maximumExpirationPeriod,
-        @ValidationMetadata(FieldType.CONFIG_LIMIT_PERIOD) @JsonProperty("replic_limit_period") String replicLimitPeriod,
-        @ValidationMetadata(FieldType.CONFIG_LIMIT_COUNT) @JsonProperty("replic_limit_count") Integer replicLimitCount,
+        @ValidationMetadata(value = FieldType.CONFIG_MAX_EXP, required = false) @JsonProperty("maximum_expiration_period") String maximumExpirationPeriod,
+        @ValidationMetadata(value = FieldType.CONFIG_LIMIT_PERIOD, required = false) @JsonProperty("replic_limit_period") String replicLimitPeriod,
+        @ValidationMetadata(value = FieldType.CONFIG_LIMIT_COUNT, required = false) @JsonProperty("replic_limit_count") Integer replicLimitCount,
         @ValidationMetadata(FieldType.CONFIG_ALLOW_SIGNUP) @JsonProperty("allow_signup") boolean allowSignup
 ) {
 }
