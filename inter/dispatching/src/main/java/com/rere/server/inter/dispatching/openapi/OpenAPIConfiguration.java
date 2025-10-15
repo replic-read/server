43190c3dcdde61d29f7e7585bc8e3d0aadaa123e
Simon Bumiller <simon@bumiller.me>
2025-10-15 16:15:14 +0200

    Migrated to bean openapi metadata

--- inter/dispatching/src/main/java/com/rere/server/inter/dispatching/openapi/OpenAPIConfiguration.java
+++ inter/dispatching/src/main/java/com/rere/server/inter/dispatching/openapi/OpenAPIConfiguration.java
@@ -1,37 +1,43 @@
 package com.rere.server.inter.dispatching.openapi;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.annotations.OpenAPI30;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SecurityScheme(
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT",
-        name = "jwt"
-)
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Replic-Read REST-API",
-                description = "This is the open-api documentation for the REST-API exposed by the backend server of the Replic-Read system",
-                contact = @Contact(
-                        email = "simon@bumiller.me",
-                        name = "Simon Bumiller",
-                        url = "https://github.com/SimonBumiller"
-                ),
-                license = @License(
-                        name = "Apache-2.0 license",
-                        url = "https://www.apache.org/licenses/LICENSE-2.0"
-                ),
-                version = "1.0.0"
-        ),
-        security = @SecurityRequirement(name = "jwt")
-)
-@OpenAPI30
+import java.util.List;
+
+/**
+ * Configuration that provides the open api metadata.
+ */
+@Configuration
 public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Replic-Read REST-API")
+                        .description("This is the open-api documentation for the REST-API exposed by the backend server of the Replic-Read system")
+                        .contact(new Contact()
+                                .email("simon@bumiller.me")
+                                .name("Simon Bumiller")
+                                .url("https://github.com/SimonBumiller"))
+                        .license(new License()
+                                .name("Apache-2.0 license")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .components(new Components()
+                        .addSecuritySchemes("jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                )
+                .security(List.of(new SecurityRequirement().addList("jwt")));
+    }
+
 }
