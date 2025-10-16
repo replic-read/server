1b45ab07fd7fab3e33c42c5b6fc48ae548829d4a
Simon Bumiller <simon@bumiller.me>
2025-10-16 16:07:33 +0200

    Added dev profile CORS configuration which is less restrictive

--- inter/dispatching/src/main/java/com/rere/server/inter/dispatching/security/DevelopmentWebMvcConfig.java
+++ inter/dispatching/src/main/java/com/rere/server/inter/dispatching/security/DevelopmentWebMvcConfig.java
@@ -1,17 +1,20 @@
-package com.rere.server.inter.dispatching;
+package com.rere.server.inter.dispatching.security;
 
 import org.springdoc.webmvc.api.OpenApiWebMvcResource;
 import org.springdoc.webmvc.ui.SwaggerConfigResource;
 import org.springdoc.webmvc.ui.SwaggerWelcomeWebMvc;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.function.Predicate;
 
+@Profile("dev")
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class DevelopmentWebMvcConfig implements WebMvcConfigurer {
 
     /**
      * The base rest path for our controllers.
@@ -26,5 +29,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix(BASE_PATH_PREFIX, predicate);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
